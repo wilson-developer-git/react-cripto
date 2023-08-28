01b3e50b784ff943ea7418d47fd284230fb9b689
Wilson <wilsonprogram@gmail.com>
2023-08-28 10:58:51 -0300

    Criando a pagina home e sua tabela de detalhes de moedas

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,50 @@
+import { Link } from 'react-router-dom'
+import  styles  from './index.module.css'
+import { BiSearch } from 'react-icons/bi'
 
 export function Home(){
     return(
-        <h1>Home</h1>
+        <main className={styles.container}>
+            <form className={styles.form}>
+                <input 
+                    placeholder="Dígite o simbolo da moeda: BTC..."
+                />
+                <button type="submit">
+                    <BiSearch size={30} color="#fff"/>
+                </button>
+            </form>
+
+            <table>
+                <thead>
+                    <tr>
+                       <th scope="col">Moeda</th> 
+                       <th scope="col">Valor mercado</th> 
+                       <th scope="col">Preço</th> 
+                       <th scope="col">Volume</th> 
+                    </tr>
+                </thead>
+
+                <tbody id="tbody">
+                    <tr className={styles.tr}>
+                        <td className={styles.tdLabel} data-label="Moeda">
+                            <Link className={styles.link} to="/detail/btc">
+                                <span>Bitcoin</span> | BTC
+                            </Link>
+                        </td>
+                        <td className={styles.tdLabel} data-label="Mercado">
+                            R$ 19293
+                        </td>
+                        <td className={styles.tdLabel} data-label="Preço">
+                            R$ 40.962
+                        </td>
+                        <td className={styles.tdLoss} data-label="Volume">
+                            <span>-5.3</span>
+                        </td>
+                    </tr>
+                </tbody>
+
+            </table>
+
+        </main>
     )
 }
